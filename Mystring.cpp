6aa2593c4addb4bd6c81399f3942068629a6e16d
Liam Dillingham <liam.g.dillingham@hpe.com>
2016-11-07 15:48:57 -0600

    Latest Update
    The ostream overloader isn't working rn

--- Mystring.cpp
+++ Mystring.cpp
@@ -1,7 +1,7 @@
 #include "Mystring.h"
 
 //Default Constructor
-Mystring::Mystring() { /// READY
+Mystring::Mystring() {
     ptr_buffer = new char[1];
     *ptr_buffer = '\0';
     len = 0;
@@ -9,7 +9,7 @@ Mystring::Mystring() { /// READY
 }
 
 //Initialized Constructor
-Mystring::Mystring(const char *s) { /// READY
+Mystring::Mystring(const char *s) {
     len = strlen(s);
     buf_size = len + 1;
     ptr_buffer = new char[buf_size];
@@ -17,7 +17,7 @@ Mystring::Mystring(const char *s) { /// READY
 }
 
 //Copy Constructor
-Mystring::Mystring(const Mystring &orig) {  /// READY
+Mystring::Mystring(const Mystring &orig) {
     len = orig.length();
     buf_size = len + 1;
     ptr_buffer = new char[buf_size];
@@ -25,36 +25,40 @@ Mystring::Mystring(const Mystring &orig) {  /// READY
 }
 
 //Destructor
-Mystring::~Mystring() { /// READY
+Mystring::~Mystring() {
     delete[] ptr_buffer;
     len = 0;
     buf_size = 0;
 }
 
 //Change buffer size to n
-void Mystring::reserve(size_type n) { /// READY
+void Mystring::reserve(size_type n) {
     if(buf_size >= n)
         return;
     buf_size = n;
+    char *temp = ptr_buffer;
+    ptr_buffer = new char[buf_size];
+    strcpy(ptr_buffer, temp);
+    delete[] temp;
 }
 
 //Get size of array
-Mystring::size_type Mystring::size() const { /// READY
+Mystring::size_type Mystring::size() const { //////// DONE
     return len;
 }
 
 //Return # of characters minus '\0'
-Mystring::size_type Mystring::length() const { /// READY
+Mystring::size_type Mystring::length() const { //////// DONE
     return len;
 }
 
 //Return size of array
-Mystring::size_type Mystring::capacity() const { /// READY
+Mystring::size_type Mystring::capacity() const { //////// DONE
     return buf_size;
 }
 
 //Return maximum size of array
-Mystring::size_type Mystring::max_size() const { /// READY
+Mystring::size_type Mystring::max_size() const { //////// DONE
     return npos - 4;
 }
 
@@ -70,19 +74,18 @@ bool Mystring::empty() const { /// READY
 //**************************
 
 //Copy Assignment
-Mystring& Mystring::operator=(const Mystring &obj) { /// READY
-    ptr_buffer = obj.ptr_buffer;
-    buf_size = obj.buf_size;
+Mystring& Mystring::operator=(const Mystring &obj) { //////// DONE
+    reserve(obj.len);
+    strcpy(ptr_buffer, obj.ptr_buffer);
     len = obj.len;
     return *this;
 }
 
 //Copy Assignment
-Mystring& Mystring::operator=(const char *str) { /// READY
-    len = strlen(str);
-    buf_size = len + 1;
-    ptr_buffer = new char[buf_size];
+Mystring& Mystring::operator=(const char *str) { //////// DONE
+    reserve(strlen(str));
     strcpy(ptr_buffer, str);
+    len = strlen(str);
     return *this;
 }
 
@@ -92,17 +95,21 @@ char Mystring::operator[](size_type pos) const { /// READY
     return *(ptr_buffer + pos);
 }
 
-char& Mystring::operator[](size_type pos) { /// READY
+char& Mystring::operator[](size_type pos) {
     return *(ptr_buffer + pos);
 }
 
 // Append
-Mystring& Mystring::operator+=(const Mystring& obj) { /// READY
+Mystring& Mystring::operator+=(const Mystring& obj) {
+    len = len + obj.len;
+    reserve(len);
     strcat(ptr_buffer, obj.ptr_buffer);
     return *this;
 }
 
-Mystring& Mystring::operator+=(const char * str) { /// READY
+Mystring& Mystring::operator+=(const char * str) {
+    len = len + strlen(str);
+    reserve(len);
     strcat(ptr_buffer, str);
     return *this;
 }
@@ -112,7 +119,7 @@ Mystring& Mystring::operator+=(const char * str) { /// READY
 //**************************
 
 //Clear the contents
-void Mystring::clear() { /// READY
+void Mystring::clear() {
     delete[] ptr_buffer;
     ptr_buffer = new char[1];
     *ptr_buffer = '\0';
@@ -121,59 +128,67 @@ void Mystring::clear() { /// READY
 }
 
 //Expand string by 1 (proportionally) and add a single new character on the end.
-void Mystring::push_back(char c) { /// Ready
-    len++, buf_size++;
-    char *tempPtr = new char[len];
-    tempPtr[len] = c;
-    tempPtr[buf_size] = '\0';
-    ptr_buffer = tempPtr;
+void Mystring::push_back(char c) {
+    buf_size++;
+    reserve(buf_size);
+    ptr_buffer[len] = c;
+    ptr_buffer[buf_size] = '\0';
+    len++;
 }
 
 //(OBJECT) concatenate a string of size n >= 0 to the end
-Mystring& Mystring::append(const Mystring &obj) {  /// READY
+Mystring& Mystring::append(const Mystring &obj) {
+    len = len + obj.len;
+    reserve(len);
     strcat(ptr_buffer, obj.ptr_buffer);
     return *this;
 }
 
 //(POINTER) concatenate a string of size n >= 0 to the end
-Mystring& Mystring::append(const char * str) {  /// READY
+Mystring& Mystring::append(const char * str) {
+    len = len + strlen(str);
+    reserve(len);
     strcat(ptr_buffer, str);
     return *this;
 }
 
 //(OBJECT) Insert character into the string
-Mystring& Mystring::insert(size_type pos, const Mystring& str) {  /// READY
-    //Mystring *newMystring = new char[first.length() + second.length() + 1];
-    char *temp = ptr_buffer + pos;
-    strcpy(ptr_buffer + pos, str.c_str());
-    strcat(ptr_buffer, temp);
+Mystring& Mystring::insert(size_type pos, const Mystring& obj) {
+    len = len + obj.len;
+    reserve(len);
+    Mystring temp = ptr_buffer + pos;
+    strcpy(ptr_buffer + pos, obj.c_str());
+    append(temp);
     return *this;
 }
 
 //(POINTER) Insert character into the string
 Mystring& Mystring::insert(size_type pos, const char * str) { /// READY
-    char *temp = ptr_buffer + pos;
+    len = len + strlen(str);
+    reserve(len);
+    Mystring temp = ptr_buffer + pos;
     strcpy(ptr_buffer + pos, str);
-    strcat(ptr_buffer, temp);
+    append(temp);
     return *this;
 }
 
 //(OBJECT) Replace some characters in a string
-Mystring& Mystring::replace(size_type start, size_type span, const Mystring& str) {
-
-    //Find a way to deallocate SPAN spaces within the original string, and replace with new string
-    //I.e. similar to INSERT, but with a truncation included.
-    char *temp = ptr_buffer + start;
-    strcpy(ptr_buffer + start, str.c_str());
-    strcat(ptr_buffer, temp);
+Mystring& Mystring::replace(size_type start, size_type span, const Mystring& obj) {
+    len = len + obj.len;
+    reserve(len);
+    Mystring temp(ptr_buffer + start + span);
+    strcpy(ptr_buffer + start, obj.ptr_buffer);
+    append(temp);
     return *this;
 }
 
 //(POINTER) Replace some characters in a string
 Mystring& Mystring::replace(size_type start, size_type span, const char * str) {
-    char *temp = ptr_buffer + start;
+    len = len + strlen(str);
+    reserve(len);
+    Mystring temp(ptr_buffer + start + span);
     strcpy(ptr_buffer + start, str);
-    strcat(ptr_buffer, temp);
+    append(temp);
     return *this;
 }
 
@@ -182,18 +197,30 @@ Mystring& Mystring::replace(size_type start, size_type span, const char * str) {
 //**************************
 
 //Convert to Character array
-const char* Mystring::c_str() const { /// Ready
+const char* Mystring::c_str() const {
     return ptr_buffer;
 }
 
 //(OBJECT) Find beginning of string
-Mystring::size_type Mystring::find_first_of(const Mystring& str, size_type pos) const { // we can kill 0 if we want
-
+Mystring::size_type Mystring::find_first_of(const Mystring& str, size_type pos) const {
+    for(size_t i = pos; i < len; i++) {
+        for(size_t j = 0; j < str.len; j++) {
+            if(*(ptr_buffer + i) == *(str.ptr_buffer + j))
+                return i;
+        }
+    }
+    return npos;
 }
 
 //(POINTER) Find beginning of string
-Mystring::size_type Mystring::find_first_of(const char* str, size_type pos) const {
-
+Mystring::size_type Mystring::find_first_of(const char* str, size_type pos) const { /// READY
+    for(size_t i = pos; i < len; i++) {
+        for(size_t j = 0; j < strlen(str); j++) {
+            if(*(ptr_buffer + i) == *(str + j))
+                return i;
+        }
+    }
+    return npos;
 }
 
 
@@ -208,18 +235,18 @@ Mystring::size_type Mystring::find_first_of(const char* str, size_type pos) cons
 //**************************
 
 //(OBJECT <-> OBJECT) equality
-bool operator==(const Mystring& left, const Mystring& right) { /// Ready
-    return strcmp(left.c_str(), right.c_str());
+bool operator==(const Mystring& left, const Mystring& right) {
+    return !strcmp(left.c_str(), right.c_str());
 }
 
 //(POINTER <-> OBJECT) equality
-bool operator==(const char *left, const Mystring& right) { /// Ready
-    return strcmp(left, right.c_str());
+bool operator==(const char *left, const Mystring& right) {
+    return !strcmp(left, right.c_str());
 }
 
 //(POINTER <-> POINTER) equality
-bool operator==(const Mystring& left, const char *right) { /// Ready
-    return strcmp(left.c_str(), right);
+bool operator==(const Mystring& left, const char *right) {
+    return !strcmp(left.c_str(), right);
 }
 
 //**************************
@@ -227,17 +254,17 @@ bool operator==(const Mystring& left, const char *right) { /// Ready
 //**************************
 
 //(OBJECT <-> OBJECT) inequality
-bool operator!=(const Mystring& left, const Mystring& right) {  /// Ready
+bool operator!=(const Mystring& left, const Mystring& right) {
     return strcmp(left.c_str(), right.c_str());
 }
 
 //(POINTER <-> OBJECT) inequality
-bool operator!=(const char *left, const Mystring& right) { /// Ready
+bool operator!=(const char *left, const Mystring& right) {
     return strcmp(left, right.c_str());
 }
 
 //(POINTER <-> POINTER) inequality
-bool operator!=(const Mystring& left, const char *right) { /// Ready
+bool operator!=(const Mystring& left, const char *right) {
     return strcmp(left.c_str(), right);
 }
 
@@ -246,10 +273,9 @@ bool operator!=(const Mystring& left, const char *right) { /// Ready
 //**************************
 
 //Add two strings together end to beginning
-Mystring operator+(const Mystring& first, const Mystring& second) { /// Ready
-    char *temp = new char[first.length() + second.length() + 1];
-    strcpy(temp, first.c_str());
-    strcat(temp, second.c_str());
+Mystring operator+(const Mystring& first, const Mystring& second) {
+    Mystring temp;
+    temp = first + second;
     return temp;
 }
 
@@ -258,8 +284,9 @@ Mystring operator+(const Mystring& first, const Mystring& second) { /// Ready
 //**************************
 
 //Print out a character string
-ostream& operator<<(ostream& out, const Mystring& str) { /// Ready
-    out << str.c_str();
+ostream& operator<<(ostream& out, const Mystring& obj) {
+    while(char *i)
+        out << obj.ptr_buffer[i];
     return out;
 }
 
--- main.cpp
+++ main.cpp
@@ -19,5 +19,45 @@ int main()
     Mystring s2 = " Dillingham";
     check(s1, "s1");
     check(s2, "s2");
+    cout << "BREAK" << endl << endl;
 
+    s1 += s2;
+    check(s1, "s1");
+    s2 = s1;
+    check(s2, "s2");
+    s2 = " Sponsky";
+    check(s2, "s2");
+    //check(s2[0], "s2");
+
+    s1 += ", a college student";
+    s2 = s1;
+    s2 = "Liam Dillingham, aasdf";
+    check(s1, "s1");
+    s1.clear();
+    check(s1, "s1");
+    s1.push_back('.');
+    check(s1, "s1");
+    check(s2, "s2");
+    s2.append(s1);
+    check(s2, "s2");
+    s2.append(" At the U of A.");
+    check(s2, "s2");
+    s2.insert(5, "Gilchrist");
+    check(s2, "s2");
+    s2.insert(14, s1);
+    check(s2, "s2");
+    s2.replace(4, 3, s1);
+    check(s2, "s2");
+    s2.replace(4, 3, "woo");
+    check(s2, "s2");
+
+    s1 = "test";
+    s2 = "test";
+    if(s1 == s2)
+        cout << "equal\n";
+    s1 = "t";
+    if(s1 != s2)
+        cout << "not equal\n";
+    Mystring s3 = s1 + s2;
+    cout << s3;
 }
